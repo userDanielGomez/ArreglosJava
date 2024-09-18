12d3c81db4c16f748cdc4d64b5721f6693e83ba6
userDanielGomez <101981448+userDanielGomez@users.noreply.github.com>
2024-09-18 00:52:53 -0600

    Primer commit

---
+++ Ej1.java
@@ -0,0 +1,23 @@
+public class Ej1 {
+  
+  public static void main(String [] args){
+     System.out.println("Hola");
+
+    // Crear un vector de cinco posiciones, posteriormente guardar un numero en
+    // cada una de las posiciones del vector, y finalmente imprimir en pantalla
+    // cada una de las posiciones para verificar que se hayan guardado los 
+    // números de manera correcta
+
+    int arreglo[] = new int[5];
+
+    arreglo[0]=1;
+    arreglo[1]=2;
+    arreglo[2]=3;
+    arreglo[3]=4;
+    arreglo[4]=5;
+
+    System.out.println(arreglo[0]+" "+arreglo[1]+" "+arreglo[2]+" "+arreglo[3]+" "+arreglo[4]);
+
+
+  }
+}
---
+++ Ej2.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Ej2{
+  
+  public static void main(String[] args) {
+    // Realizar un programa que contenga un vector cuya longitud sea 
+    // proporcionado por el usuario, despues solicitar al usuario los
+    // números necesarios para llenar todas las posiciones del vector, es
+    // decir, si el usuario solicitó un arreglo de 10 dimensiones, entonces
+    // el programa deberá de solicitarle al usuario 10 números, y 
+    // finalmente imprimir el arreglo en pantalla de la siguiente manera
+    // [5] [54] [12] [65] [6] [54] [897]
+
+    int longitud = 0;
+    //Creación de objeto Scanner para leer la entrada de teclado
+    Scanner entrada = new Scanner(System.in);
+    //Lectura y validación de la variable longitud
+    while (true) {
+      try{
+        System.out.println("Ingrese la longitud del vector a crear");
+        longitud = Integer.parseInt(entrada.nextLine());
+        if(longitud>0)
+          break;
+      }catch(Exception e){
+        System.out.println("El valor debe ser un numero entero");
+      }
+    }
+
+    int arreglo[] = new int[longitud];
+
+    //Bucle para obtener y almacenar los valores del arreglo
+    for(int i=0; i<longitud; i++){
+      while (true) {
+        try{
+          System.out.println("Ingrese el valor a almacenar en la posición "+i);
+          arreglo[i]=Integer.parseInt(entrada.nextLine());
+          break;
+        }catch(Exception e){
+          System.out.println("El valor debe ser númerico");
+        }
+      }
+    }
+
+    //Bucle para recorrer el arreglo almacenado
+    for(int i = 0; i<longitud; i++){
+      System.out.print("["+arreglo[i]+"]");
+    }
+
+    entrada.close();
+  }
+
+}
